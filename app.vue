e8b0c19668a5d361c4a492a823ca596f0c63780b
Santiago Quipildor <quipildorsantiago@gmail.com>
2022-11-14 00:46:16 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="font-nunito text-red-50">xdasd</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    configPath: '~/config/tailwind.js',
+  },
+  app: {
+    head: {
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        { rel: 'preconnect', href:"https://fonts.googleapis.com" },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOriginIsolated: true },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap' },
+      ],
+    },
+  },
+});
+
